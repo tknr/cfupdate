5964148e0b77cc6c54d92aa8416c704d7e606630
tknr <geishagirlskoume@gmail.com>
2018-12-14 01:54:33 +0900

    2018年 12月 14日 金曜日 01:53:45 JST

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-ln -s `pwd`/opti_picture.sh /usr/local/bin/opti_picture
+#!/bin/bash -x
+cp cfupdate.service /etc/systemd/system/
+cp cfupdate.timer /etc/systemd/system/
+cp cfupdator /usr/local/bin/
+chmod +x /usr/local/bin/cfupdator
 
+systemctl enable cfupdate.timer
+systemctl start cfupdate.timer
